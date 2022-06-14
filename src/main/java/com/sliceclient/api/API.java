fa4a50aedb83343b3f22aab97bf84587d3e90aff
NickRest <nickrestemail@gmail.com>
2022-06-14 15:28:16 -0400

    Working Auth

--- src/main/java/com/sliceclient/api/API.java
+++ src/main/java/com/sliceclient/api/API.java
@@ -10,7 +10,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @Getter @Setter
